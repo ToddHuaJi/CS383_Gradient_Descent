b4214007e9f1f6dda2c83ce8fdc63023c4038da0
ToddYang <yanggt97@gmail.com>
2018-04-02 21:44:32 -0400

    Update my_csp.py
    debugging

--- HW3/HW3/code/my_csp.py
+++ HW3/HW3/code/my_csp.py
@@ -17,12 +17,12 @@ class CSP(object):
             domains.append([1,2,3,4,5,6,7,8,9])
 
         self.variables = variables
-        self.domains = domains                              #81 of range(1,10), representing total choice for all cells
+        self.domains = domains                              #81 of range(1,10), representing total chioce for all cells
 
 
     def constraints(self):
         '''
-        Your choice on how to implement constraints.
+        Your chioce on how to implement constraints.
         HINT: You should define constraints such they are fast to evaluate. One method is to define the constraints as a
                 function of the row and column. Sets are a fast way to remove duplicates.
         :return: False if one spec is not met, true if all specs met, runtime is low when board is not complete
@@ -79,12 +79,14 @@ class CSP_Solver(object):
         You should run on the hard puzzles to make sure you aren't violating corner cases that come up.
         Harder puzzles will take longer to solve.
         :param puzzle_file: the puzzle file to solve
+
         '''
         self.sudoku = Sudoku(puzzle_file) # this line has to be here to initialize the puzzle
         vars = np.zeros(shape = [9,9], dtype = int)
         #vars = self.sudoku.load_board(puzzle_file)      #read board config into vars
         # you can use these if you want
         self.num_guesses = 0
+
 
         for i in range(0,9):
             vars[i] = self.sudoku.load_board(puzzle_file)[i]      #read board config into vars
@@ -112,8 +114,12 @@ class CSP_Solver(object):
         Return None, number of guesses no solution is found
         :return: tuple (list of list (ie [[]]), number of guesses
         '''
+        found = False
+        while not found:
+            csp, found = self.recursive_backtracking(self.sudoku, self.csp)
 
-        return self.csp.variables, self.csp.domains
+
+        return self.csp.variables, self.num_guesses
 
     def backtracking_search(self, sudoku, csp):
         '''
@@ -124,6 +130,7 @@ class CSP_Solver(object):
         :param csp: CSP class instance
         :return: board state (list of lists), num guesses
         '''
+
         return self.recursive_backtracking(sudoku, csp), self.num_guesses+1
 
     def recursive_backtracking(self, sudoku, csp):
@@ -134,30 +141,29 @@ class CSP_Solver(object):
         :param csp: CSP class instance
         :return: search result T or F
         '''
-        value = 0
-        if csp.constraints():                       #iff complete and OK then quit
-            return csp
+        board = csp.variables
+        self.num_guesses = self.num_guesses + 1
+        if self.csp.constraints():                       #iff complete and OK then quit
+            return csp, True
 
-        row, col = self.select_unassigned_var(csp.variables)        #next empty cell
-        if row == 10 or col == 10:
-            if csp.constraints():                       #iff complete and OK then quit
-                return csp, True
-            else:
-                return csp, False                        #board not satisfying constraints, next try
-        else:
-            while self.consistent(row, col, value, csp.variables):      #find a consistent value for this cell
-                value = self.Order_Domain_Values(col,row,csp)
-            csp.variables[row][col] = value             #update
+        print(csp.variables)
+        if self.num_guesses == 2:
 
-            recursive_backtracking(self. sudoku, csp)
+            exit()
+        print(self.num_guesses)
+        chioce = self.chioce_list(board)                  #chioce list
+        row, col, num = self.select_unassigned_var(chioce)      #vector of cell and possible chioces
+        for x in num:
+            if self.consistent(row, col, x, board):       #just pick a consistent num an plug it in
+                csp.variables[row][col] = x               #assign
+                self.recursive_backtracking(self. sudoku, csp)
 
-
-
+        csp.variables[row][col] = 0                       #reset the board
         return csp, False
 
     def Order_Domain_Values(self , col, row, csp):
         cell = col + row*9                              #which cell we are talking about
-        value = csp.domains[cell][0]                    #pick a num from the possible choice and remove it
+        value = csp.domains[cell][0]                    #pick a num from the possible chioce and remove it
         csp.domains[cell],remove(value)
 
 
@@ -165,22 +171,26 @@ class CSP_Solver(object):
 
 
 
-    def select_unassigned_var(self, board):
+    def select_unassigned_var(self, chioce):
         '''
         find the next empty cell
         :param board: list of lists
-        :return: coordinates of the empty cell, or 10, 10 for NOT FOUND
+        :return: coordinates of the empty cell with fewest chioce
         '''
         #scan through the list and return first cell with 0
-        found = True
-        for i in range(0,9):            #row
-            for j in range(0,9):        #col
-                if board[i][j] == 0:
-                    found = False
-                    return [i,j]       #return
-        if not found:
-            return [10,10]
+        cell = []                       #possible chioce for the fewest cells
+        location = -1                   #location of the cell
+        lens = 9
+        for i in range(0,9*9):
+            if lens > len(chioce[i]):
+                lens = len(chioce[i])
+                location = i
+                cell = chioce[i]
 
+        row = location // 9
+        col = location - row*9
+
+        return row, col, cell
 
     def consistent(self, row,col , value, board):
         '''
@@ -193,7 +203,6 @@ class CSP_Solver(object):
         :return: True if consistent False otherwise
         '''
         cons = True                               #default True
-
         block_r = row // 3                        #calculate start r of block
         """
         Three checks:
@@ -203,13 +212,12 @@ class CSP_Solver(object):
         """
         if value == 0 :
             cons == False
-            break
+
         else:
             for a in range(0,9):                                    # row check
                 if value == board[row][a] :
                     cons = False
                     break
-
             for b in range(0,9):                                    # col check
                 if value == board[b][col]:
                     cons = False
@@ -219,13 +227,54 @@ class CSP_Solver(object):
                 for c in range(0,3):
                     if value == board[block_r*3+r][block_c*3+c]:
                         cons = False
-                        break
+                    break
 
         return cons
 
+    def chioce_list(self, board):
+        """
+        Return a list of chioces for all cells, after assigning a specific cell
+        """
+        chioce = []
 
 
+        for i in range(0,9*9):
+            chioce.append([1,2,3,4,5,6,7,8,9])
+        for r in range(0,9):                    #get rid of in place chioce
+            for c in range(0,9):
+                if not board[r][c] == 0:
+                    chioce = self.removechioce(chioce, r, c, board)
 
+        return chioce
+
+    def removechioce(self, chioce, r, c, board):
+        """
+        remove relevant chioce regarding [r,c] in the list
+        """
+        value = board[r][c]
+        chioce[r*9+c] = [0]
+        for a in chioce[r*9:r*9+9]:         #the row
+            try:
+                a.remove(value)
+            except ValueError:
+                pass
+
+        for b in range(0,9):                #the col
+            try:
+                chioce[b*9+c].remove(value)
+            except ValueError:
+                pass
+
+        block_r = r//3
+        block_c = c//3
+        for i in range(0,3):                #the block
+            for j in range(0,3):
+                try:
+                    chioce[block_c*3+j+(block_r*3+i)*9].remove(value)
+                except ValueError:
+                    pass
+
+        return chioce
 
 if __name__ == '__main__':
     csp_solver = CSP_Solver('puz-001.txt')
